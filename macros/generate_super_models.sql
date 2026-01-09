e39832e3dc992be18669d04a9543903dfcfa8c8d
ic-corraldata <isaac@corraldata.com>
2026-01-09 21:30:14 +0000

    update macro name and logs

--- macros/generate_super_models.sql
+++ macros/generate_super_models.sql
@@ -1,4 +1,4 @@
-{% macro generate_super_plan(table) %}
+{% macro generate_super_models(table) %}
     {% set supers = get_super_columns(table) %}
     {% if supers | length == 0 %}
         {% do log("No SUPER columns found.", info=True) %}
@@ -11,16 +11,16 @@
     {% do log("Table: " ~ table, info=True) %}
     {% do log("Detected SUPER columns: " ~ supers | join(', '), info=True) %}
 
-    {% do log("===== MODEL: " ~ plan.parent.name ~ " =====", info=True) %}
+    {% do log("===== PARENT MODEL =====", info=True) %}
     {% do log(render_parent_model(plan.parent), info=True) %}
 
     {% for child in plan.children %}
-        {% do log("===== MODEL: " ~ child.name ~ " =====", info=True) %}
+        {% do log("===== CHILD MODEL: " ~ child.name ~ " =====", info=True) %}
         {% do log(render_child_model(child), info=True) %}
     {% endfor %}
 
     {% do log("===== SCHEMA YAML =====", info=True) %}
-    {% do log(render_schema_yaml(plan), info=True) %}
+    {% do log(render_yaml(plan), info=True) %}
 
     {{ return("Plan generated") }}
 {% endmacro %}
